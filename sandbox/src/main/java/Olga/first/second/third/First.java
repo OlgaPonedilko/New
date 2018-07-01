9695a683a9df389c2736f534bc4e72a0274e8295
OlgaPonedilko <40152270+OlgaPonedilko@users.noreply.github.com>
2018-07-01 15:41:35 -0200

    Points

--- sandbox/src/main/java/Olga/first/second/third/First.java
+++ sandbox/src/main/java/Olga/first/second/third/First.java
@@ -8,6 +8,8 @@ Square s = new Square (8);
     System.out.println(+s.l+"="+s.area());
     Rectungle r = new Rectungle (9, 13);
     System.out.println(+r.a+"*"+r.b+"="+r.area());
+    Point p = new Point (398, 76);
+    System.out.println(+p.p1+"-"+p.p2+"="+p.area());
 
 }
 
---
+++ sandbox/src/main/java/Olga/first/second/third/Point.java
@@ -0,0 +1,13 @@
+package Olga.first.second.third;
+
+public class Point {
+    public double p1;
+    public double p2;
+    public Point (double p1, double p2) {
+        this.p1=p1;
+        this.p2=p2;
+    }
+    public double area() {
+        return this.p1-this.p2;
+    }
+}
